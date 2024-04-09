783a603dfecd3863319b2f7cfccb541ffbda8e02
linxingyu2023 <792344805@qq.com>
2024-04-09 15:16:10 +0800

    fix:TileLayer组件修改

--- src/maptalks/components/layers/MtTileLayer/index.vue
+++ src/maptalks/components/layers/MtTileLayer/index.vue
@@ -61,8 +61,24 @@ export default defineComponent({
     // 获取上级组件中的地图对象
     let maptalks = inject("maptalks", null);
     let map = maptalks.value;
-    // 定义瓦片图层组对象
-    let tileLayer = null;
+    // 获取坐标系
+    let proj = props.projection ? props.projection : "EPSG:4326";
+    // 获取图层ID
+    let id = props.id ? props.id : uuidv4().replace(/-/g, "");
+    // 接收图层配置信息并初始化图层对象
+    let tileLayer = new TileLayer(id, {
+      tileSystem: [1, -1, -180, 90],
+      spatialReference: {
+        projection: proj
+      },
+      urlTemplate: props.urlTemplate,
+      maxAvailableZoom: 18,
+      subdomains: ["1", "2", "3", "4", "5"],
+      visible: props.visible,
+      minZoom: props.minZoom,
+      maxZoom: props.maxZoom,
+      zIndex: props.zIndex
+    });
     // 监听瓦片图层ID
     watch(
       props.id,
@@ -73,22 +89,12 @@ export default defineComponent({
       },
       { immediate: true }
     );
-    // 监听瓦片图层开关
-    watch(
-      props.visible,
-      (newVal, oldVal) => {
-        if (tileLayer && tileLayer.isLoaded()) {
-          tileLayer.setOpacity(newVal);
-        }
-      },
-      { immediate: true }
-    );
     // 监听瓦片图层透明度
     watch(
       props.opacity,
       (newVal, oldVal) => {
         if (tileLayer && tileLayer.isLoaded()) {
-          tileLayer.setZIndex(newVal);
+          tileLayer.setOpacity(newVal);
         }
       },
       { immediate: true }
@@ -119,24 +125,6 @@ export default defineComponent({
 
     // 添加瓦片图层
     const addTileLayer = () => {
-      // 获取坐标系
-      let proj = props.projection ? props.projection : "EPSG:4326";
-      // 获取图层ID
-      let id = props.id ? props.id : uuidv4().replace(/-/g, "");
-      // 接收图层配置信息并初始化图层对象
-      tileLayer = new TileLayer(id, {
-        tileSystem: [1, -1, -180, 90],
-        spatialReference: {
-          projection: proj
-        },
-        urlTemplate: props.urlTemplate,
-        maxAvailableZoom: 18,
-        subdomains: ["1", "2", "3", "4", "5"],
-        visible: props.visible,
-        minZoom: props.minZoom,
-        maxZoom: props.maxZoom,
-        zIndex: props.zIndex
-      });
       // 判断更多图层...
       const groupGLLayer = inject("groupGLLayer");
       // const groupTileLayer = inject("groupTileLayer");
