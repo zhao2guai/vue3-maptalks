a2d3ef39c13b0fd5cb9c958425959c84a68c0bfd
赵二乖 <1090685714@qq.com>
2024-04-17 18:10:51 +0800

    fix: 修改包的引入方式;

--- src/maptalks/components/index.ts
+++ src/maptalks/components/index.ts
@@ -5,18 +5,12 @@
  * @LastEditors: 赵二乖
  * @LastEditTime: 2024-03-28 09:01:16
  */
-import type { App, Plugin } from "vue";
-import * as Map from "./map";
-import * as Layers from "./layers";
-import * as GLTF from './gltf';
+import Map from "./map";
+import Layers from "./layers";
+import GLTF from './gltf';
 
-const install: Plugin = (app: App) => {
-  app.use(Map.install);
-  app.use(Layers.install);
-  app.use(GLTF.install);
-};
-
-const plugin = { install };
+const plugin = { Map, Layers, GLTF  };
 
 export default plugin;
-export { install, Map, Layers, GLTF };
+
+export { Map, Layers, GLTF };
