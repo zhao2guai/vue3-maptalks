25a003aded84dfe63d547ab4222fdad804ec00cc
赵二乖 <1090685714@qq.com>
2024-05-10 10:47:19 +0800

    fix: 新增关于页面及其组件的引入,需要从新install安装;

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,9 +1,40 @@
+<template>
+  <div></div>
+</template>
+
 <script setup lang="ts">
+import { ref } from "vue";
 defineOptions({
   name: "Welcome"
 });
 </script>
 
-<template>
-  <h1>Pure-Admin-Thin（非国际化版本）</h1>
-</template>
+<style lang="scss" scoped>
+:deep(.el-card) {
+  --el-card-border-color: none;
+
+  /* 解决概率进度条宽度 */
+  .el-progress--line {
+    width: 85%;
+  }
+
+  /* 解决概率进度条字体大小 */
+  .el-progress-bar__innerText {
+    font-size: 15px;
+  }
+
+  /* 隐藏 el-scrollbar 滚动条 */
+  .el-scrollbar__bar {
+    display: none;
+  }
+
+  /* el-timeline 每一项上下、左右边距 */
+  .el-timeline-item {
+    margin: 0 6px;
+  }
+}
+
+.main-content {
+  margin: 20px 20px 0 !important;
+}
+</style>
